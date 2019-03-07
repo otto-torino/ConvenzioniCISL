ac541024c637dcbab91baa42a20645680407b8d8
abidibo <abidibo@gmail.com>
2019-03-07 11:02:21 +0100

    first commit

---
+++ App/Themes/Images.js
@@ -0,0 +1,27 @@
+// leave off @2x/@3x
+const images = {
+  logo: require('../Images/app-logo.png'),
+  otto: require('../Images/Otto.png'),
+  rtpComunicazione: require('../Images/RTPComunicazione.jpg'),
+  clearLogo: require('../Images/top_logo.png'),
+  launch: require('../Images/launch-icon.png'),
+  ready: require('../Images/app-logo.png'),
+  ignite: require('../Images/ignite_logo.png'),
+  igniteClear: require('../Images/ignite-logo-transparent.png'),
+  tileBg: require('../Images/tile_bg.png'),
+  background: require('../Images/BG.png'),
+  buttonBackground: require('../Images/button-bg.png'),
+  api: require('../Images/Icons/icon-api-testing.png'),
+  components: require('../Images/Icons/icon-components.png'),
+  deviceInfo: require('../Images/Icons/icon-device-information.png'),
+  faq: require('../Images/Icons/faq-icon.png'),
+  home: require('../Images/Icons/icon-home.png'),
+  theme: require('../Images/Icons/icon-theme.png'),
+  usageExamples: require('../Images/Icons/icon-usage-examples.png'),
+  chevronRight: require('../Images/Icons/chevron-right.png'),
+  hamburger: require('../Images/Icons/hamburger.png'),
+  backButton: require('../Images/Icons/back-button.png'),
+  closeButton: require('../Images/Icons/close-button.png')
+}
+
+export default images
